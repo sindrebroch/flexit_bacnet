14609166872deade8d608140f99a7a921afc842a
Sindre Broch <sindrebroch@gmail.com>
2022-11-21 16:19:51 +0100

    Add diagnostics

---
+++ custom_components/flexit_bacnet/diagnostics.py
@@ -0,0 +1,25 @@
+"""Diagnostics support for Flexit."""
+
+from __future__ import annotations
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from custom_components.flexit.api import FlexitApiClient
+
+from custom_components.flexit.coordinator import FlexitDataUpdateCoordinator
+from custom_components.flexit.models import FlexitSensorsResponse
+
+from .const import DOMAIN
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry
+) -> dict:
+    """Return diagnostics for a config entry."""
+
+    coordinator: FlexitDataUpdateCoordinator = hass.data[DOMAIN][config_entry.entry_id]
+
+    return {
+        "flexit_bacnet": str(coordinator.device),
+    }
